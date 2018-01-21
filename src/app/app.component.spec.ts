868efd09d34aae2386a2ceed2211192031d2077f
Eismunt Tatyana <po4ta6402@gmail.com>
2018-01-21 16:41:18 +0200

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    console.log(app.title);
+    expect(app.title).toEqual('app');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    const compiled = fixture.debugElement.nativeElement;
+    app.title = 999;
+    fixture.detectChanges();
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    // expect(compiled.querySelector('h2').innerText).toBe('Here are some links to help you start:');
+  }));
+});
